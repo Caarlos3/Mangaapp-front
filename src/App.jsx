eaa4d1732660f288574345a6ae08c6be67f26cb5
Caarlos3 <crt.981995@gmail.com>
2025-12-08 20:07:39 +0100

    Modificado input

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -7,27 +6,65 @@ function App() {
       <div
         style={{
           display: "flex",
-          margin: "40px",
+          margin: "100px",
           alignItems: "center",
           height: "100vh",
           flexDirection: "column",
         }}
       >
-        <div className="search-bar">
-          <input
-            type="text"
-            placeholder="Search 🔎"
-            style={{
-              width: "300px",
-              height: "40px",
-              borderRadius: "20px",
-              border: "none",
-              padding: "0 20px",
-              fontSize: "16px",
-              textAlign: "center",
-              outline: "none"
-            }}
-          />
+        <div className="grid"></div>
+        <div id="poda">
+          <div className="glow"></div>
+          <div className="darkBorderBg"></div>
+          <div className="darkBorderBg"></div>
+          <div className="darkBorderBg"></div>
+
+          <div className="white"></div>
+          <div className="border"></div>
+
+          <div id="main">
+            <input
+              placeholder="Search..."
+              type="text"
+              name="text"
+              className="input"
+            />
+            <div id="input-mask"></div>
+            <div id="pink-mask"></div>
+
+            <div id="search-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                height="24"
+                fill="none"
+                className="feather feather-search"
+              >
+                <circle stroke="url(#search)" r="8" cy="11" cx="11"></circle>
+                <line
+                  stroke="url(#searchl)"
+                  y2="16.65"
+                  y1="22"
+                  x2="16.65"
+                  x1="22"
+                ></line>
+                <defs>
+                  <linearGradient gradientTransform="rotate(50)" id="search">
+                    <stop stopColor="#f8e7f8" offset="0%"></stop>
+                    <stop stopColor="#b6a9b7" offset="50%"></stop>
+                  </linearGradient>
+                  <linearGradient id="searchl">
+                    <stop stopColor="#b6a9b7" offset="0%"></stop>
+                    <stop stopColor="#837484" offset="50%"></stop>
+                  </linearGradient>
+                </defs>
+              </svg>
+            </div>
+          </div>
         </div>
       </div>
     </>
